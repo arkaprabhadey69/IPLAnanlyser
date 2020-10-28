88e415cdfacb0edd05922f4c2528e72f64c584fa
arkaprabhadey <euphoricdey420@gmail.com>
2020-10-28 12:22:17 +0530

    [Arka]Added:Method to get batsman with highest average and strikerate

--- src/main/java/com/bl/ipl/IPLAnalyser.java
+++ src/main/java/com/bl/ipl/IPLAnalyser.java
@@ -115,6 +115,25 @@ public class IPLAnalyser {
 
 
     }
+    public String getPlayersWithTopSRandAverage() throws IPLException {
+        try (Writer writer = new FileWriter("./src/test/resources/IPLBattingSRandAvg.json")) {
+            if (IPLCSVList == null || IPLCSVList.size() == 0) {
+                throw new IPLException("No data", IPLException.ExceptionType.NO_DATA);
+            }
+            Comparator<IPLRuns> iplComparator = Comparator.comparing(IPLRuns::getAvg).thenComparing(ipl->ipl.strikeRate);
+            this.descendingSort(iplComparator);
+            String json = new Gson().toJson(IPLCSVList);
+            Gson gson = new GsonBuilder().create();
+            gson.toJson(IPLCSVList, writer);
+            return json;
+
+        } catch (RuntimeException | IOException e) {
+            throw new IPLException(e.getMessage(),
+                    IPLException.ExceptionType.FILE_OR_HEADER_PROBLEM);
+        }
+
+
+    }
 
     private void descendingSort(Comparator<IPLRuns> iplComparator) {
         for (int i = 0; i < IPLCSVList.size() - 1; i++) {
